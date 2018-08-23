b51291ba2a84e6711d2117efce17a60bf96ba6a9
Tiago Schenkel <tiago.schenkel@gmail.com>
2018-08-23 22:28:06 +0200

    add minimal interfaces to abstract an generic display list structure

---
+++ src/robotlegs/bender/extensions/displayList/api/IDisplayObject.ts
@@ -0,0 +1,13 @@
+// ------------------------------------------------------------------------------
+//  Copyright (c) 2017-present, RobotlegsJS. All Rights Reserved.
+//
+//  NOTICE: You are permitted to use, modify, and distribute this file
+//  in accordance with the terms of the license agreement accompanying it.
+// ------------------------------------------------------------------------------
+
+import { IDisplayObjectContainer } from "./IDisplayObjectContainer";
+
+export let IDisplayObject = Symbol("IDisplayObject");
+export interface IDisplayObject {
+    parent: IDisplayObjectContainer;
+}
---
+++ src/robotlegs/bender/extensions/displayList/api/IDisplayObjectContainer.ts
@@ -0,0 +1,18 @@
+// ------------------------------------------------------------------------------
+//  Copyright (c) 2017-present, RobotlegsJS. All Rights Reserved.
+//
+//  NOTICE: You are permitted to use, modify, and distribute this file
+//  in accordance with the terms of the license agreement accompanying it.
+// ------------------------------------------------------------------------------
+
+import { IDisplayObject } from "./IDisplayObject";
+
+export let IDisplayObjectContainer = Symbol("IDisplayObjectContainer");
+export interface IDisplayObjectContainer extends IDisplayObject {
+    children?: IDisplayObject[];
+
+    numChildren?: number;
+    getChildAt?(index: number): IDisplayObject;
+
+    contains(child: IDisplayObject): boolean;
+}
